e5ef4187a898177e3e4c5ce4cbeba8a563f44902
Katya <pinkgirl06@mail.ru>
2022-06-15 08:13:53 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.fairy-tail__slider, .our-trip__slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/right.svg" alt="1"></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/left.svg" alt="2"></button>',
+        autoplay: true,
+        fade: true
+    });
+});
